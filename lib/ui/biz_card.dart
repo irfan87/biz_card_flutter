ac1b0b20e56e91b782718628d0fc09735a646e71
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-06-01 18:59:02 +0800

    done bizcard creation

--- lib/ui/biz_card.dart
+++ lib/ui/biz_card.dart
@@ -3,6 +3,81 @@ import 'package:flutter/material.dart';
 class BizCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'BizCard',
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: <Widget>[_getCard(), _getAvatar()],
+        ),
+      ),
+    );
+  }
+
+  Container _getCard() {
+    return Container(
+      width: 350,
+      height: 200,
+      margin: const EdgeInsets.all(50.0),
+      decoration: BoxDecoration(
+          color: Colors.red, borderRadius: BorderRadius.circular(14.5)),
+      padding: const EdgeInsets.only(top: 24),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: Text(
+              'Ahmad Irfan Mohammad Shukri',
+              style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Icon(Icons.email),
+                Text('irfanshukri203@gmail.com'),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Icon(Icons.phone),
+                Text('[Your phone number]'),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Container _getAvatar() {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(50.0)),
+          border: Border.all(color: Colors.redAccent, width: 1.2),
+          image: DecorationImage(
+              image: NetworkImage('https://picsum.photos/300/300'),
+              fit: BoxFit.cover)),
+    );
   }
 }
